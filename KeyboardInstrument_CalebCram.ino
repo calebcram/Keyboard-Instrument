b99b6f059949e13ea0f562c64e652bcc34f382a9
calebcram <49875750+calebcram@users.noreply.github.com>
2021-07-18 15:38:27 -0600

    Initial Commit

---
+++ KeyboardInstrument_CalebCram.ino
@@ -0,0 +1,32 @@
+#include "pitches.h"
+int notes [] = {NOTE_C3, NOTE_D2, NOTE_G3, NOTE_D5};
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int keyVal = analogRead(A0);
+  Serial.println(keyVal);
+  if(keyVal == 1023)
+  {
+    tone(8, notes[0]);
+  }
+  else if(keyVal >= 990 && keyVal <= 1010)
+  {
+    tone(8, notes[1]);
+  }
+  else if(keyVal >= 505 && keyVal <= 515)
+  {
+    tone(8, notes[2]);
+  }
+  else if(keyVal >= 5 && keyVal <= 10)
+  {
+    tone(8, notes[3]);
+  }
+  else
+  {
+    noTone(8);
+  }
+}
